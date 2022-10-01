88fdc4f8496d09c4531aa4fd3fdc4142026ecb11
tesfalem tekie <teshanna72016@gmail.com>
2022-09-30 22:14:54 -0500

    map-filter-reduce exercise completed

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,57 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const arrayOfUserObjects = users.filter(user => user.languages.length >= 3);
+const arrayOfStrings = users.map(user => user.email);
+const average = users.reduce((totalYearsOfExperience, user) => {
+        return totalYearsOfExperience + user.yearsOfExperience / users.length;
+}, 0);
+
+const longestEmail = users.reduce((longest, user) =>{
+    if (user.email.length > longest.length) {
+        longest = user.email;
+    }
+    return longest;
+}, '');
+
+const usersNameInSingleString = users.reduce((userName, user) =>{
+    userName.push(user.name);
+    return userName;
+}, []);
+usersNameInSingleString.join(', ');
+
